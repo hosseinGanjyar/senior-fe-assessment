695754a3421a76a2fd5a0e8fa5d7364f433614eb
hossein.ganjyar@gmail.com <hossein.ganjyar@gmail.com>
2024-03-21 18:13:51 +0330

    #feat: add component assigned with its route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
+import { AssignedComponent } from './components/assigned/assigned.component';
 import { ImagesComponent } from './components/images/images.component';
 
-export const routes: Routes = [{
-    path: 'images',
-    component: ImagesComponent
-}];
+export const routes: Routes = [
+    {
+        path: 'images',
+        component: ImagesComponent
+
+    },
+    {
+        path: 'assigned',
+        component: AssignedComponent
+
+    }
+];
---
+++ src/app/components/assigned/assigned.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assigned',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './assigned.component.html',
+  styleUrl: './assigned.component.scss'
+})
+export class AssignedComponent {
+  images: string[] = [
+    'https://getbootstrap.com/docs/5.3/examples/features/unsplash-photo-1.jpg',
+    'https://getbootstrap.com/docs/5.3/examples/features/unsplash-photo-2.jpg',
+    'https://getbootstrap.com/docs/5.3/examples/features/unsplash-photo-3.jpg',
+  ];
+
+}
