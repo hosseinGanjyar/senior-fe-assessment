69bda2a9d8f98fc0572c52167e7fc1c82ffcded3
hossein.ganjyar@gmail.com <hossein.ganjyar@gmail.com>
2024-03-22 19:01:33 +0330

    #feat(unAssigned): dedicate unassigned images by a new component

---
+++ src/app/components/un-assigned/un-assigned.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-un-assigned',
+  standalone: true,
+  imports: [],
+  templateUrl: './un-assigned.component.html',
+  styleUrl: './un-assigned.component.scss'
+})
+export class UnAssignedComponent {
+
+}
